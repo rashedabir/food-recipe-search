5c38ec68f52bd8ad27b67e3bfe78e376c3907a6d
Rashed Abir <rashed15-2155@diu.edu.bd>
2021-06-03 12:11:56 +0600

    first commit

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -10,7 +10,7 @@ function Foods() {
         <h3 className="text-center">All Foods</h3>
         {Object.keys(foods).length > 1 ? (
           <h4> Total Found : {foods.length} </h4>
-        ) : null}
+        ) : <h4>Not Found</h4>}
       </div>
       <div className="row row-cols-1 row-cols-md-2 g-4 p-2 m-auto d-flex justify-content-center">
         {Object.keys(foods).length === 0
